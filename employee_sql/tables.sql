b968aa26eed1bae21016c34caa41b302efe137f6
jgutier151 <jgutierrezcb@gmail.com>
2023-11-05 18:38:00 -0600

    sql challenge

---
+++ employee_sql/tables.sql
@@ -0,0 +1,49 @@
+CREATE TABLE departments (
+    dept_no VARCHAR(50) PRIMARY KEY,
+    dept_name VARCHAR(50) NOT NULL
+);
+copy departments FROM '/Users/macbook_air/Documents/UTA-VIRT-DATA-PT-08-2023-U-LOLC/09-SQL/sql-challenge/Starter_Code/data/departments.csv'
+Delimiter ','
+CSV HEADER;
+CREATE TABLE titles (
+    title_id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
+copy titles FROM '/Users/macbook_air/Documents/UTA-VIRT-DATA-PT-08-2023-U-LOLC/09-SQL/sql-challenge/Starter_Code/data/titles.csv'
+Delimiter ','
+CSV HEADER;
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(10) REFERENCES titles(title_id),
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL
+);
+copy employees FROM '/Users/macbook_air/Documents/UTA-VIRT-DATA-PT-08-2023-U-LOLC/09-SQL/sql-challenge/Starter_Code/data/employees.csv'
+Delimiter ','
+CSV HEADER;
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY,
+    salary INT NOT NULL
+);
+copy salaries FROM '/Users/macbook_air/Documents/UTA-VIRT-DATA-PT-08-2023-U-LOLC/09-SQL/sql-challenge/Starter_Code/data/salaries.csv'
+Delimiter ','
+CSV HEADER;
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(50) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+copy dept_emp FROM '/Users/macbook_air/Documents/UTA-VIRT-DATA-PT-08-2023-U-LOLC/09-SQL/sql-challenge/Starter_Code/data/dept_emp.csv'
+Delimiter ','
+CSV HEADER;
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(50) REFERENCES departments(dept_no),
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no)
+);
+copy dept_manager FROM '/Users/macbook_air/Documents/UTA-VIRT-DATA-PT-08-2023-U-LOLC/09-SQL/sql-challenge/Starter_Code/data/dept_manager.csv'
+Delimiter ','
+CSV HEADER;
